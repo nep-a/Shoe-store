83d5c3fcdaf8273689f975df9d60161307209e38
nep-a <musalakaniwitaba@gmail.com>
2025-10-21 15:30:42 -0700

    added template folder to store my html files

--- shoe/models.py
+++ shoe/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Products(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=150)
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+    image = models.ImageField(upload_to='shoe/')
+
+    def __str__(self):
+        return self.title
